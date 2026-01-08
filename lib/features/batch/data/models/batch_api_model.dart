3600629aca9071f7fa9e90563091ac6bc12948b0
asrim10 <asrimmsuwall@gmail.com>
2026-01-08 22:55:47 +0545

    batch_api_model

---
+++ lib/features/batch/data/models/batch_api_model.dart
@@ -0,0 +1,43 @@
+import 'package:lost_n_found/features/batch/domain/entities/batch_entity.dart';
+
+class BatchApiModel {
+  final String? id;
+  final String batchName;
+  final String? status;
+
+  BatchApiModel({required this.batchName, this.id, this.status});
+  //toJSON
+  Map<String, dynamic> toJson() {
+    return {"batchName": batchName};
+  }
+
+  //fromJSON
+  factory BatchApiModel.fromJson(Map<String, dynamic> json) {
+    return BatchApiModel(
+      id: json['_id'] as String,
+      batchName: json["batchName"] as String,
+      status: json['status'] as String,
+    );
+  }
+
+  // toEntity
+  BatchEntity toEntity() {
+    return BatchEntity(
+      batchId: id,
+      batchName: "UK - $batchName",
+      status: status,
+    );
+  }
+
+  //fromEntity
+  factory BatchApiModel.fromEntity(BatchEntity entity) {
+    return BatchApiModel(batchName: entity.batchName);
+  }
+
+  //toEntityList
+  static List<BatchEntity> toEntityList(List<BatchApiModel> models) {
+    return models.map((model) => model.toEntity()).toList();
+  }
+
+  //fromEntityList
+}
