15462596598f9ded1cadd1dcc89926a381e49f41
asrim10 <asrimmsuwall@gmail.com>
2026-01-21 18:15:59 +0545

    upload_photo_usecase

---
+++ lib/features/item/domain/usecases/upload_photo_usecase.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lost_n_found/core/error/failures.dart';
+import 'package:lost_n_found/core/usecases/app_usecases.dart';
+import 'package:lost_n_found/features/item/data/repositories/item_repository.dart';
+import 'package:lost_n_found/features/item/domain/repositories/item_repository.dart';
+
+final uploadPhotoUsecaseProvider = Provider<UploadPhotoUsecase>((ref) {
+  final repository = ref.read(itemRepositoryProvider);
+  return UploadPhotoUsecase(repository: repository);
+});
+
+class UploadPhotoUsecase implements UsecaseWithParms<String, File> {
+  final IItemRepository _repository;
+
+  UploadPhotoUsecase({required IItemRepository repository})
+    : _repository = repository;
+  @override
+  Future<Either<Failure, String>> call(File params) {
+    return _repository.uploadImage(params);
+  }
+}
